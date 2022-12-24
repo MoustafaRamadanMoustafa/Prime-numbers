b4e8156ccc061f798608447926691f2fa4c0a069
MoustafaRamadanMoustafa <121277027+MoustafaRamadanMoustafa@users.noreply.github.com>
2022-12-24 23:42:21 +0200

    Add files via upload
    Prime_numApp

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime_numbers
+{
+    class Program
+    {
+        static bool isPrime(int n)
+        {
+            if (n == 1 || n == 0) return false;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int N = 18;
+            for (int i = 5; i <= N; i++)
+            {
+                if (isPrime(i))
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
